b5be62d77b64aaa353ff9843b87ce6573557e997
kiran <kirankumargangisetty7@gmail.com>
2025-04-30 15:30:04 +0530

    Update index.js

--- frontend/src/components/ProductDetails/index.js
+++ frontend/src/components/ProductDetails/index.js
@@ -50,7 +50,7 @@ class ProductDetails extends Component {
       apiStatus: apiStatusConstants.inProgress,
     })
     const jwtToken = localStorage.getItem('jwt_token')
-    const apiUrl = `http://localhost:3000/product/${id}`
+    const apiUrl = `https://shoppy-mobi.onrender.com/product/${id}`
     const options = {
       headers: {
         Authorization: `Bearer ${jwtToken}`,
