81e501591215c842790f9d0caba2019fd17ff554
kiran <kirankumargangisetty7@gmail.com>
2025-04-30 15:34:06 +0530

    Update index.js

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -6,7 +6,7 @@ const Header = props => {
   const onClickLogout = async (orders, cartList) => {
     const { history } = props;
 
-    const url = 'http://localhost:3000/storeUserData'; // Backend API endpoint
+    const url = 'https://shoppy-mobi.onrender.com/storeUserData'; // Backend API endpoint
     const requestData = {
       orders,
       cartList,
